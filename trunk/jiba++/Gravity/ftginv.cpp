fd90a18a40555987c309ffdd818ca95d85102dbd
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2008-09-01 11:25:47 +0000

    synchronization commit, the inversion programs need some cleaning

--- trunk/jiba++/Gravity/ftginv.cpp
+++ trunk/jiba++/Gravity/ftginv.cpp
@@ -9,13 +9,38 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "../Global/convert.h"
 #include "ThreeDGravityModel.h"
 #include "../Inversion/MatrixTools.h"
 #include "ReadWriteGravityData.h"
 #include <boost/numeric/bindings/atlas/cblas2.hpp>
 #include "../Inversion/LinearInversion.h"
+#include "../ModelBase/VTKTools.h"
 
 namespace atlas = boost::numeric::bindings::atlas;
+
+void SumSensitivitiesAndPlot(const jiba::rmat &Sens, const size_t startindex,
+    const std::string &filename, const jiba::ThreeDGravityModel &Model)
+  {
+    const size_t nmod = Sens.size2();
+    const size_t ndata = Sens.size1();
+    jiba::rvec SummedSens(nmod);
+
+    SummedSens *= 0.0;
+    for (size_t i = startindex; i < ndata; i += 9)
+      {
+        SummedSens += boost::numeric::ublas::matrix_row<const jiba::rmat>(Sens,
+            i);
+      }
+    //for output we copy the summed sensitivities into a model type structure
+    jiba::ThreeDModelBase::t3DModelData
+        SensModel(
+            boost::extents[Model.GetDensities().shape()[0]][Model.GetDensities().shape()[1]][Model.GetDensities().shape()[2]]);
+    std::copy(SummedSens.begin(), SummedSens.end(), SensModel.data());
+
+    jiba::Write3DModelToVTK(filename, "summed_sens", Model.GetXCellSizes(),
+        Model.GetYCellSizes(), Model.GetZCellSizes(), SensModel);
+  }
 
 int main(int argc, char *argv[])
   {
@@ -33,13 +58,16 @@ int main(int argc, char *argv[])
     std::cout << "Data Filename: ";
     std::cin >> datafilename;
     jiba::ReadTensorGravityMeasurements(datafilename, Data, PosX, PosY, PosZ);
-    jiba::rvec DataVector(Data.size() * 9);
+    jiba::rvec DataVector(Data.size() * 9), DataError(Data.size() * 9);
     for (size_t i = 0; i < Data.size(); ++i)
       {
         copy(Data.at(i).data().begin(), Data.at(i).data().end(),
             DataVector.begin() + i * 9);
       }
-
+    for (size_t i = 0; i < DataVector.size(); ++i)
+      {
+        DataError(i) = 0.02 * DataVector(i);
+      }
     const size_t nmeas = PosX.size();
 
     //set the measurement points in the model to those of the data
@@ -53,6 +81,12 @@ int main(int argc, char *argv[])
     Model.CalcTensorGravity();
     Model.SaveTensorMeasurements(modelfilename + ".new.nc");
     jiba::rmat Sensitivities(Model.GetTensorSensitivities());
+    //write out sensitivities for the 9 tensor elements
+    for (size_t i = 0; i < 9; ++i)
+      {
+        SumSensitivitiesAndPlot(Sensitivities, i, modelfilename + stringify(i)
+            + ".vtk", Model);
+      }
     //depth weighting
     double z0;
     std::cout << "Enter z0: ";
@@ -76,7 +110,7 @@ int main(int argc, char *argv[])
     std::cin >> evalthresh;
     jiba::rvec InvModel;
 
-    jiba::DataSpaceInversion(Sensitivities, DataVector, ModelWeight,
+    jiba::DataSpaceInversion()(Sensitivities, DataVector, ModelWeight,DataError,
         evalthresh, InvModel);
 
     std::copy(InvModel.begin(), InvModel.end(), Model.SetDensities().origin());
