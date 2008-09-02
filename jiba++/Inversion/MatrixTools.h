d849bf0f96b6fa6347079f43210e79bacb2d0c83
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2008-09-02 15:04:39 +0000

    made DataSpaceInversion a class and added comments

--- jiba++/Inversion/MatrixTools.h
+++ jiba++/Inversion/MatrixTools.h
@@ -10,8 +10,12 @@
 
 #include "../Global/VecMat.h"
 
+
 namespace jiba
   {
+    /** \addtogroup inversion General routines for inversion */
+    /* @{ */
+
     //! Calculate the singular value decomposition for a given matrix
     /*! Calculate the singular value decomposition using lapack
      * @param SensitivityMatrix The n*m input matrix, will be modified during the calculation
@@ -23,16 +27,34 @@ namespace jiba
       {
         const size_t size1 = SensitivityMatrix.size1();
         const size_t size2 = SensitivityMatrix.size2();
-        s.resize(std::min(size1,size2));
+        s.resize(std::min(size1, size2));
         vt.resize(size2, size2);
         u.resize(size1, size1);
         boost::numeric::bindings::lapack::gesvd(SensitivityMatrix, s, u, vt);
       }
-
+    //! Invert a square Matrix using LU-Factorization
     bool InvertMatrix(const jiba::rmat& input, jiba::rmat& inverse);
 
-    void GeneralizedInverse(const rmat &Input, rmat &Inverse, const double lowthresh = 0.0, const double upthresh = 1.0);
-
+    //! Implements the generalized inverse through truncated SVD
+    class GeneralizedInverse
+      {
+    private:
+      rvec s;
+      rmat u;
+      rmat vt;
+      rmat G;
+    public:
+      //! return the vector of generalized eigenvalues, no threshold is applied
+      const rvec &GetEigenvalues() const {return s;}
+      //! return the data eigenvectors after application of the thresholds
+      const rmat &GetDataVectors() const {return u;}
+      //! return the model eigenvectors after application of the thresholds
+      const rmat &GetModelVectors() const {return vt;}
+      //! The core calculation routine to calculate the generalized inverse
+      void operator()(const rmat &Input, rmat &Inverse, const double lowthresh = 0.0,
+          const double upthresh = 1.0);
+      };
+    /* @} */
   }
 
 #endif /* MATRIXTOOLS_H_ */
