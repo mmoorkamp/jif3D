8395430f91d90b1e3160bfad033833d587cd9910
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2008-09-05 12:46:31 +0000

    added modelspace inversion and lagrange multiplier lambda

--- jiba++/Inversion/LinearInversion.cpp
+++ jiba++/Inversion/LinearInversion.cpp
@@ -17,14 +17,14 @@ namespace jiba
 
     void DataSpaceInversion::operator()(rmat &Sensitivities,
         rvec &Data, const rvec &WeightVector, const rvec &DataError,
-        const double evalthresh, rvec &InvModel)
+        const double evalthresh, const double lambda, rvec &InvModel)
       {
         const size_t nmeas = Data.size();
         const size_t nparm = Sensitivities.size2();
         for (size_t i = 0; i < nmeas; ++i)
           {
             boost::numeric::ublas::matrix_row<jiba::rmat>(
-                        Sensitivities, i) /= DataError(i);
+                Sensitivities, i) /= DataError(i);
             Data(i) /= DataError(i);
           }
 
@@ -41,6 +41,10 @@ namespace jiba
         jiba::rmat Gamma(nmeas, nmeas);
         atlas::gemm(CblasNoTrans, CblasNoTrans, 1.0, Sensitivities,
             FilteredSens, 0.0, Gamma);
+        for (size_t i = 0; i < nmeas; ++i)
+          {
+            Gamma(i,i) += lambda;
+          }
 
         jiba::rmat DataInverse(nmeas, nmeas);
         jiba::GeneralizedInverse()(Gamma, DataInverse, evalthresh, 1.0);
@@ -49,6 +53,47 @@ namespace jiba
 
         jiba::rmat ModelInverse(nparm, nmeas);
         atlas::gemm(CblasNoTrans, CblasNoTrans, 1.0, FilteredSens, DataInverse,
+            0.0, ModelInverse);
+
+        atlas::gemv(ModelInverse, Data, InvModel);
+      }
+
+    void ModelSpaceInversion::operator()(rmat &Sensitivities,
+        rvec &Data, const rvec &WeightVector, const rvec &DataError,
+        const double evalthresh, const double lambda, rvec &InvModel)
+      {
+        const size_t nmeas = Data.size();
+        const size_t nparm = Sensitivities.size2();
+        for (size_t i = 0; i < nmeas; ++i)
+          {
+            boost::numeric::ublas::matrix_row<jiba::rmat>(
+                Sensitivities, i) /= sqrt(DataError(i));
+            Data(i) /= sqrt(DataError(i));
+          }
+
+        //FilteredSens.resize(Sensitivities.size2(),Sensitivities.size1());
+        //FilteredSens.assign(trans(Sensitivities));
+        //for (size_t i = 0; i < nparm; ++i)
+         // {
+          //  boost::numeric::ublas::matrix_row<jiba::rmat> CurrentRow(
+           //     FilteredSens, i);
+//
+  //          CurrentRow /= WeightVector(i);
+    //      }
+
+        jiba::rmat Gamma(nparm, nparm);
+        atlas::gemm(CblasTrans, CblasNoTrans, 1.0,
+            Sensitivities, Sensitivities, 0.0, Gamma);
+        for (size_t i = 0; i < nparm; ++i)
+          Gamma(i,i) += lambda * 1./WeightVector(i);
+
+        jiba::rmat Inverse(nparm, nparm);
+        jiba::GeneralizedInverse()(Gamma, Inverse, evalthresh, 1.0);
+
+        InvModel.resize(nparm);
+
+        jiba::rmat ModelInverse(nparm, nmeas);
+        atlas::gemm(CblasNoTrans, CblasTrans, 1.0, Inverse, Sensitivities,
             0.0, ModelInverse);
 
         atlas::gemv(ModelInverse, Data, InvModel);
--- jiba++/Inversion/test_inversion.cpp
+++ jiba++/Inversion/test_inversion.cpp
@@ -9,72 +9,131 @@
 #define BOOST_TEST_MAIN ...
 #include <boost/test/included/unit_test.hpp>
 #include <stdlib.h>
+#include <algorithm>
+#include <numeric>
 #include "MatrixTools.h"
+#include "LinearInversion.h"
 #include <boost/test/floating_point_comparison.hpp>
 
 BOOST_AUTO_TEST_SUITE( Inversion_Test_Suite )
 
 //test that the SVD fulfills the absolute minimum requirement
 //calculate the SVD of an Identity matrix
-BOOST_AUTO_TEST_CASE(basic_svd_test)
-  {
-    const size_t msize = 4;
-    jiba::rmat TestMatrix(msize,msize);
-    TestMatrix *= 0.0;
-    for (size_t i = 0; i < msize; ++i)
-      {
-        TestMatrix(i,i) = 1.0;
-      }
-    jiba::rvec s;
-    jiba::rmat u,vt;
-    jiba::SVD(TestMatrix,s,u,vt);
-    for (size_t i = 0; i < msize; ++i)
-      {
-        BOOST_CHECK_CLOSE(s(i),1.0,std::numeric_limits<float>::epsilon());
-        BOOST_CHECK_CLOSE(u(i,i),1.0,std::numeric_limits<float>::epsilon());
-        BOOST_CHECK_CLOSE(vt(i,i),1.0,std::numeric_limits<float>::epsilon());
-      }
+BOOST_AUTO_TEST_CASE  (basic_svd_test)
+    {
+      srand(time(NULL));
+      const size_t msize = 4;
+      jiba::rmat TestMatrix(msize,msize);
+      std::fill_n(TestMatrix.data().begin(),msize*msize,0.0);
+      for (size_t i = 0; i < msize; ++i)
+        {
+          TestMatrix(i,i) = 1.0;
+        }
+      jiba::rvec s;
+      jiba::rmat u,vt;
+      jiba::SVD(TestMatrix,s,u,vt);
+      for (size_t i = 0; i < msize; ++i)
+        {
+          BOOST_CHECK_CLOSE(s(i),1.0,std::numeric_limits<float>::epsilon());
+          BOOST_CHECK_CLOSE(u(i,i),1.0,std::numeric_limits<float>::epsilon());
+          BOOST_CHECK_CLOSE(vt(i,i),1.0,std::numeric_limits<float>::epsilon());
+        }
 
-  }
+    }
+  //test that the determinant of and identity matrix is 1
+  BOOST_AUTO_TEST_CASE (basic_determinant_test)
+    {
+      const size_t msize = 4;
+      jiba::rmat TestMatrix(msize,msize);
+      std::fill_n(TestMatrix.data().begin(),msize*msize,0.0);
+      for (size_t i = 0; i < msize; ++i)
+        {
+          TestMatrix(i,i) = 1.0;
+        }
+      double det = jiba::Determinant(TestMatrix);
+      BOOST_CHECK_CLOSE(det,1.0,std::numeric_limits<float>::epsilon());
 
-BOOST_AUTO_TEST_CASE(invert_matrix_test)
-  {
-    const size_t msize = 4;
-    jiba::rmat TestMatrix(msize, msize);
-    jiba::rmat InverseMatrix(msize, msize);
-    srand(time(NULL));
-    for (size_t i = 0; i < msize; ++i)
+    }
+
+  //the determinant should be the product of the eigenvalues
+  //because we use an SVD, it will always be positive,
+  //so we take the absolute value
+  BOOST_AUTO_TEST_CASE (determinant_test)
+    {
+      const size_t msize = 4;
+      jiba::rmat TestMatrix(msize,msize);
+      for (size_t i = 0; i < msize; ++i)
       for (size_t j = 0; j < msize; ++j)
-        TestMatrix(i, j) = double(rand());
+      TestMatrix(i, j) = double(rand() % 10);
 
-    jiba::InvertMatrix(TestMatrix, InverseMatrix);
-    jiba::rmat MultMatrix(boost::numeric::ublas::prec_prod(TestMatrix,
-        InverseMatrix));
-    for (size_t i = 0; i < msize; ++i)
-      {
-        BOOST_CHECK_CLOSE(MultMatrix(i, i), 1.0,
-            std::numeric_limits<float>::epsilon());
-      }
-  }
+      double det = jiba::Determinant(TestMatrix);
+      jiba::rvec s;
+      jiba::rmat u,vt;
+      jiba::SVD(TestMatrix,s,u,vt);
+      double sdet = std::accumulate(s.begin(),s.end(),1.0,std::multiplies<double>());
+      std::cout << det << " " << sdet << std::endl;
+      BOOST_CHECK_CLOSE(det, fabs(sdet),
+          1e-4);
+    }
 
-BOOST_AUTO_TEST_CASE(generelized_inverse_test)
-  {
-    const size_t msize = 4;
-    jiba::rmat TestMatrix(msize, msize);
-    jiba::rmat InverseMatrix(msize, msize);
-    srand(time(NULL));
-    for (size_t i = 0; i < msize; ++i)
+  BOOST_AUTO_TEST_CASE(invert_matrix_test)
+    {
+      const size_t msize = 4;
+      jiba::rmat TestMatrix(msize, msize);
+      jiba::rmat InverseMatrix(msize, msize);
+      for (size_t i = 0; i < msize; ++i)
       for (size_t j = 0; j < msize; ++j)
-        TestMatrix(i, j) = double(rand());
+      TestMatrix(i, j) = double(rand());
 
-    jiba::GeneralizedInverse()(TestMatrix, InverseMatrix);
-    jiba::rmat MultMatrix(boost::numeric::ublas::prec_prod(TestMatrix,
-        InverseMatrix));
-    for (size_t i = 0; i < msize; ++i)
-      {
-        BOOST_CHECK_CLOSE(MultMatrix(i, i), 1.0,
-            std::numeric_limits<float>::epsilon());
-      }
-  }
+      jiba::InvertMatrix(TestMatrix, InverseMatrix);
+      jiba::rmat MultMatrix(boost::numeric::ublas::prec_prod(TestMatrix,
+              InverseMatrix));
+      for (size_t i = 0; i < msize; ++i)
+        {
+          BOOST_CHECK_CLOSE(MultMatrix(i, i), 1.0,
+              std::numeric_limits<float>::epsilon());
+        }
+    }
 
-BOOST_AUTO_TEST_SUITE_END()
+  BOOST_AUTO_TEST_CASE(generalized_inverse_test)
+    {
+      const size_t msize = 4;
+      jiba::rmat TestMatrix(msize, msize);
+      jiba::rmat InverseMatrix(msize, msize);
+      srand(time(NULL));
+      for (size_t i = 0; i < msize; ++i)
+      for (size_t j = 0; j < msize; ++j)
+      TestMatrix(i, j) = double(rand());
+
+      jiba::GeneralizedInverse()(TestMatrix, InverseMatrix);
+      jiba::rmat MultMatrix(boost::numeric::ublas::prec_prod(TestMatrix,
+              InverseMatrix));
+      for (size_t i = 0; i < msize; ++i)
+        {
+          BOOST_CHECK_CLOSE(MultMatrix(i, i), 1.0,
+              std::numeric_limits<float>::epsilon());
+        }
+    }
+
+  BOOST_AUTO_TEST_CASE(compare_inversions_test)
+    {
+      const size_t ndata =5;
+      const size_t nparam = 7;
+      jiba::rmat Sensitivities(ndata,nparam);
+      jiba::rvec DataVec(ndata), DataError(ndata);
+      jiba::rvec ModelWeight(nparam), DataSpaceInvModel(nparam), ModelSpaceInvModel(nparam);
+      const double evalthresh = 0.0001;
+      std::fill_n(DataVec.begin(),ndata,double(rand()));
+      std::fill_n(DataError.begin(),ndata,double(rand()));
+      std::fill_n(ModelWeight.begin(),nparam,double(rand()));
+      std::fill_n(Sensitivities.data().begin(),ndata*nparam,double(rand()));
+      jiba::DataSpaceInversion()(Sensitivities, DataVec, ModelWeight, DataError, evalthresh,0.0,
+          DataSpaceInvModel);
+      jiba::ModelSpaceInversion()(Sensitivities, DataVec, ModelWeight, DataError, evalthresh,0.0,
+          ModelSpaceInvModel);
+      for (size_t i = 0; i < nparam; ++i)
+        {
+          BOOST_CHECK_CLOSE(DataSpaceInvModel(i),ModelSpaceInvModel(i),std::numeric_limits<float>::epsilon());
+        }
+    }
+  BOOST_AUTO_TEST_SUITE_END()
