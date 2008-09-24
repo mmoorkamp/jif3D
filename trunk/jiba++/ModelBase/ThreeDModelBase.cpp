bc59d582d76b10a7c0298efd71dc9da225255a9e
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2008-09-24 08:22:59 +0000

    made Coordinate calculation functions const and public and associated class members mutable

--- trunk/jiba++/ModelBase/ThreeDModelBase.cpp
+++ trunk/jiba++/ModelBase/ThreeDModelBase.cpp
@@ -22,25 +22,43 @@ namespace jiba
       {
       }
 
+    boost::array<ThreeDModelBase::t3DModelData::index,3> ThreeDModelBase::FindAssociatedIndices(
+        const double xcoord, const double ycoord, const double zcoord) const
+      {
+        const int xindex = std::distance(GetXCoordinates().begin(),
+            std::lower_bound(GetXCoordinates().begin(),
+                GetXCoordinates().end(), xcoord));
+        const int yindex = std::distance(GetYCoordinates().begin(),
+            std::lower_bound(GetYCoordinates().begin(),
+                GetYCoordinates().end(), ycoord));
+        const int zindex = std::distance(GetZCoordinates().begin(),
+            std::lower_bound(GetZCoordinates().begin(),
+                GetZCoordinates().end(), zcoord));
+        boost::array<t3DModelData::index,3> idx =
+          {
+            { std::max(xindex-1,0), std::max(yindex-1,0), std::max(zindex-1,0) } };
+        return idx;
+      }
 
     void ThreeDModelBase::ReadDataFromNetCDF(const NcFile &NetCDFFile,
         const std::string &DataName, const std::string &UnitsName)
       {
-        Read3DModelFromNetCDF(NetCDFFile,DataName,UnitsName,XCellSizes,YCellSizes,ZCellSizes,Data);
+        Read3DModelFromNetCDF(NetCDFFile, DataName, UnitsName, XCellSizes,
+            YCellSizes, ZCellSizes, Data);
       }
 
     void ThreeDModelBase::WriteDataToNetCDF(NcFile &NetCDFFile,
         const std::string &DataName, const std::string &UnitsName) const
       {
-        Write3DModelToNetCDF(NetCDFFile,DataName,UnitsName,XCellSizes,YCellSizes,ZCellSizes,Data);
+        Write3DModelToNetCDF(NetCDFFile, DataName, UnitsName, XCellSizes,
+            YCellSizes, ZCellSizes, Data);
       }
-
-
 
     void ThreeDModelBase::WriteVTK(std::string filename,
         const std::string &DataName)
       {
-        Write3DModelToVTK(filename,DataName,GetXCellSizes(),GetYCellSizes(),GetZCellSizes(),GetData());
+        Write3DModelToVTK(filename, DataName, GetXCellSizes(), GetYCellSizes(),
+            GetZCellSizes(), GetData());
       }
 
   }
