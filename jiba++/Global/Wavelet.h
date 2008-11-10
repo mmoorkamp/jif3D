b28b610e38b51e41868ef4d8a4092ecd23305b50
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2008-11-10 14:15:52 +0000

    added multi-dimensional wavelet transform and associated test, made a first test implementation

--- jiba++/Global/Wavelet.h
+++ jiba++/Global/Wavelet.h
@@ -6,6 +6,8 @@
 //============================================================================
 
 #include "VecMat.h"
+#include <boost/multi_array.hpp>
+
 /*! \file Wavelet.h
  * Simple implementations of the discrete wavelet transform and its inverse based on Daubechies-4 wavelets.
  * The basic algorithm is described in Numerical Recipes in C, pp. 595-598
@@ -23,5 +25,90 @@ namespace jiba
     void WaveletTransform(jiba::rvec &Invec);
     //! Perform an inverse wavelet transform on the input vector
     void InvWaveletTransform(jiba::rvec &Invec);
-  /* @} */
+    //! Multidimensional wavelet transform
+    template<typename MultiArrayType>
+    void WaveletTransform(MultiArrayType &InArray);
+    template<typename MultiArrayType>
+    void InvWaveletTransform(MultiArrayType &InArray);
+    /* @} */
+
+    template<typename MultiArrayType>
+    void WaveletTransform(MultiArrayType &InArray)
+      {
+        const size_t nDimensions = InArray.num_dimensions();
+        const size_t nElements = std::accumulate(InArray.shape(),
+            InArray.shape() + nDimensions, 1,std::multiplies<size_t>());
+        jiba::rvec WorkVector(nElements);
+        size_t Stride = 1;
+        size_t preStride = 1;
+        for (size_t i = 0; i < nDimensions; ++i)
+          {
+            size_t DimSize = InArray.shape()[i];
+            Stride = DimSize * preStride;
+            if (InArray.shape()[i] > 4)
+              {
+                for (size_t j = 0; j < nElements; j += Stride)
+                  {
+                    for (size_t k = 0; k < preStride; ++k)
+                      {
+                        for (size_t l = j + k, m = 0; m < DimSize; ++m, l
+                            += preStride)
+                          {
+                            WorkVector( m) = *(InArray.origin() + l);
+                          }
+                        for (size_t n = DimSize; n >= 4; n >>= 1)
+                          {
+                            Daub4(WorkVector, n);
+                          }
+                        for (size_t l = j + k, m = 0; m < DimSize; ++m, l
+                            += preStride)
+                          {
+                            *(InArray.origin() + l) = WorkVector(m);
+                          }
+                      }
+                  }
+              }
+            preStride = Stride;
+          }
+      }
+
+    template<typename MultiArrayType>
+        void InvWaveletTransform(MultiArrayType &InArray)
+          {
+            const size_t nDimensions = InArray.num_dimensions();
+            const size_t nElements = std::accumulate(InArray.shape(),
+                InArray.shape() + nDimensions, 1,std::multiplies<size_t>());
+            jiba::rvec WorkVector(nElements);
+            size_t Stride = 1;
+            size_t preStride = 1;
+            for (size_t i = 0; i < nDimensions; ++i)
+              {
+                size_t DimSize = InArray.shape()[i];
+                Stride = DimSize * preStride;
+                if (InArray.shape()[i] > 4)
+                  {
+                    for (size_t j = 0; j < nElements; j += Stride)
+                      {
+                        for (size_t k = 0; k < preStride; ++k)
+                          {
+                            for (size_t l = j + k, m = 0; m < DimSize; ++m, l
+                                += preStride)
+                              {
+                                WorkVector( m) = *(InArray.origin() + l);
+                              }
+                            for (size_t n = 4; n <= DimSize; n <<= 1)
+                              {
+                                InvDaub4(WorkVector, n);
+                              }
+                            for (size_t l = j + k, m = 0; m < DimSize; ++m, l
+                                += preStride)
+                              {
+                                *(InArray.origin() + l) = WorkVector(m);
+                              }
+                          }
+                      }
+                  }
+                preStride = Stride;
+              }
+          }
   }
--- jiba++/Global/test_wavelet.cpp
+++ jiba++/Global/test_wavelet.cpp
@@ -34,10 +34,51 @@ BOOST_AUTO_TEST_CASE (wavelet_transform_pair)
       {
         const size_t length = 1024;
         jiba::rvec Vector(length);
-        std::fill_n(Vector.begin(),length,0.0);
+        std::fill_n(Vector.begin(), length, 0.0);
         Vector(4) = 1.0;
         jiba::InvWaveletTransform(Vector);
         std::ofstream outfile("wavelet.out");
-        std::copy(Vector.begin(),Vector.end(),std::ostream_iterator<double>(outfile,"\n"));
+        std::copy(Vector.begin(), Vector.end(), std::ostream_iterator<double>(
+            outfile, "\n"));
+      }
+
+    BOOST_AUTO_TEST_CASE (multi_dim_1dcomp)
+      {
+        const size_t length = 64;
+        jiba::rvec Vector(length);
+        std::generate_n(Vector.begin(), length, rand);
+        boost::multi_array<double, 1> InArray(boost::extents[length]);
+        std::copy(Vector.begin(), Vector.end(), InArray.origin());
+
+        jiba::WaveletTransform(Vector);
+        jiba::WaveletTransform(InArray);
+        for (size_t i = 0; i < length; ++i)
+          {
+            BOOST_CHECK_CLOSE(Vector(i),*(InArray.origin()+i),1e-3);
+          }
+        jiba::InvWaveletTransform(Vector);
+        jiba::InvWaveletTransform(InArray);
+        for (size_t i = 0; i < length; ++i)
+          {
+            BOOST_CHECK_CLOSE(Vector(i),*(InArray.origin()+i),1e-3);
+          }
+      }
+
+    BOOST_AUTO_TEST_CASE (multi_dim_wavelet_transform_pair)
+      {
+        //we check that a transform followed by the inverse
+        //gives back the original result
+        const size_t length = 8;
+        boost::multi_array<double, 3> InArray(
+            boost::extents[length][length][length]);
+        std::generate_n(InArray.origin(), pow(length, 3), rand);
+
+        boost::multi_array<double, 3> Original(InArray);
+        jiba::WaveletTransform(InArray);
+        jiba::InvWaveletTransform(InArray);
+        for (size_t i = 0; i < InArray.num_elements(); ++i)
+          {
+            BOOST_CHECK_CLOSE(*(Original.origin()+i),*(InArray.origin()+i),1e-3);
+          }
       }
     BOOST_AUTO_TEST_SUITE_END()
