4f0d45346a3a4dd2c43471e3efe6c4f39cfc19ac
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2008-12-10 10:09:17 +0000

    reduced duplicate code by creating separate function objects for inverse and forward transform created interface for raw c++ types

--- jiba++/Global/Wavelet.h
+++ jiba++/Global/Wavelet.h
@@ -25,90 +25,32 @@ namespace jiba
     void WaveletTransform(jiba::rvec &Invec);
     //! Perform an inverse wavelet transform on the input vector
     void InvWaveletTransform(jiba::rvec &Invec);
-    //! Multidimensional wavelet transform
+    //! Multidimensional wavelet transform on a boost multiarray type
     template<typename MultiArrayType>
     void WaveletTransform(MultiArrayType &InArray);
+    //! Multidimensional inverse wavelet transform on a boost multiarray type
     template<typename MultiArrayType>
     void InvWaveletTransform(MultiArrayType &InArray);
+    //! Multidimensional wavelet transform on raw c++ types
+    void WaveletTransform(double *InArray, const size_t *DimSizes, size_t ndim);
+    //! Multidimensional inverse wavelet transform on raw c++ types
+    void InvWaveletTransform(double *InArray, const size_t *DimSizes,
+        size_t ndim);
     /* @} */
 
     template<typename MultiArrayType>
     void WaveletTransform(MultiArrayType &InArray)
       {
-        const size_t nDimensions = InArray.num_dimensions();
-        const size_t nElements = std::accumulate(InArray.shape(),
-            InArray.shape() + nDimensions, 1,std::multiplies<size_t>());
-        jiba::rvec WorkVector(nElements);
-        size_t Stride = 1;
-        size_t preStride = 1;
-        for (size_t i = 0; i < nDimensions; ++i)
-          {
-            size_t DimSize = InArray.shape()[i];
-            Stride = DimSize * preStride;
-            if (InArray.shape()[i] > 4)
-              {
-                for (size_t j = 0; j < nElements; j += Stride)
-                  {
-                    for (size_t k = 0; k < preStride; ++k)
-                      {
-                        for (size_t l = j + k, m = 0; m < DimSize; ++m, l
-                            += preStride)
-                          {
-                            WorkVector( m) = *(InArray.origin() + l);
-                          }
-                        for (size_t n = DimSize; n >= 4; n >>= 1)
-                          {
-                            Daub4(WorkVector, n);
-                          }
-                        for (size_t l = j + k, m = 0; m < DimSize; ++m, l
-                            += preStride)
-                          {
-                            *(InArray.origin() + l) = WorkVector(m);
-                          }
-                      }
-                  }
-              }
-            preStride = Stride;
-          }
+        WaveletTransform(InArray.origin(), InArray.shape(),
+            InArray.num_dimensions());
       }
 
     template<typename MultiArrayType>
-        void InvWaveletTransform(MultiArrayType &InArray)
-          {
-            const size_t nDimensions = InArray.num_dimensions();
-            const size_t nElements = std::accumulate(InArray.shape(),
-                InArray.shape() + nDimensions, 1,std::multiplies<size_t>());
-            jiba::rvec WorkVector(nElements);
-            size_t Stride = 1;
-            size_t preStride = 1;
-            for (size_t i = 0; i < nDimensions; ++i)
-              {
-                size_t DimSize = InArray.shape()[i];
-                Stride = DimSize * preStride;
-                if (InArray.shape()[i] > 4)
-                  {
-                    for (size_t j = 0; j < nElements; j += Stride)
-                      {
-                        for (size_t k = 0; k < preStride; ++k)
-                          {
-                            for (size_t l = j + k, m = 0; m < DimSize; ++m, l
-                                += preStride)
-                              {
-                                WorkVector( m) = *(InArray.origin() + l);
-                              }
-                            for (size_t n = 4; n <= DimSize; n <<= 1)
-                              {
-                                InvDaub4(WorkVector, n);
-                              }
-                            for (size_t l = j + k, m = 0; m < DimSize; ++m, l
-                                += preStride)
-                              {
-                                *(InArray.origin() + l) = WorkVector(m);
-                              }
-                          }
-                      }
-                  }
-                preStride = Stride;
-              }
-          }
+    void InvWaveletTransform(MultiArrayType &InArray)
+      {
+        InvWaveletTransform(InArray.origin(), InArray.shape(),
+            InArray.num_dimensions());
+      }
+
+  //end of namespace jiba
   }
