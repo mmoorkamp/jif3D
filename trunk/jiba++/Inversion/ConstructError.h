62dbc31b4fc07487f5c7b6894eee910fcdae933a
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2009-06-18 14:09:45 +0000

    changed library organization to reduce dependencies, removed simplistic backtracking line search

--- trunk/jiba++/Inversion/ConstructError.h
+++ trunk/jiba++/Inversion/ConstructError.h
@@ -12,21 +12,23 @@
 #include <cassert>
 #include "../Global/VecMat.h"
 
-namespace jiba {
-  jiba::rvec ConstructError(const jiba::rvec &Data, const double errorlevel)
-    {
-      assert(errorlevel > 0.0);
-      const size_t ndata = Data.size();
-      const double maxdata = fabs(*std::max_element(Data.begin(), Data.end(),
-          jiba::absLess<double, double>()));
-      //create objects for the misfit and a very basic error estimate
-      jiba::rvec DataError(ndata);
-      for (size_t i = 0; i < ndata; ++i)
-        {
-          DataError( i) = std::max(std::fabs(Data(i)), 1e-2 * maxdata) * errorlevel;
-          assert(DataError(i) > 0.0);
-        }
-      return DataError;
-    }
-}
+namespace jiba
+  {
+    jiba::rvec ConstructError(const jiba::rvec &Data, const double errorlevel)
+      {
+        assert(errorlevel > 0.0);
+        const size_t ndata = Data.size();
+        const double maxdata = fabs(*std::max_element(Data.begin(), Data.end(),
+            jiba::absLess<double, double>()));
+        //create objects for the misfit and a very basic error estimate
+        jiba::rvec DataError(ndata);
+        for (size_t i = 0; i < ndata; ++i)
+          {
+            DataError(i) = std::max(std::fabs(Data(i)), 1e-2 * maxdata)
+                * errorlevel;
+            assert(DataError(i) > 0.0);
+          }
+        return DataError;
+      }
+  }
 #endif /* CONSTRUCTERROR_H_ */
