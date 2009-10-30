187d3fa24cdee44c2294e7f98076a18beed80486
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2009-10-30 16:24:34 +0000

    improved error construction for synthetic data

--- trunk/jiba++/Inversion/ConstructError.h
+++ trunk/jiba++/Inversion/ConstructError.h
@@ -21,20 +21,21 @@ namespace jiba
      * takes care of issues with zero or very small data.
      * @param Data The vector of data elements
      * @param errorlevel The minimum relative error for each datum
+     * @param absmin The absolute minimum data value considered for error calculation, this reduced the influence of very small data
      * @return The vector of error estimates
      */
-    jiba::rvec ConstructError(const jiba::rvec &Data, const double errorlevel)
+    jiba::rvec ConstructError(const jiba::rvec &Data, const double relerror, const double absmin = 0.0)
       {
         assert(errorlevel > 0.0);
         const size_t ndata = Data.size();
-        const double maxdata = fabs(*std::max_element(Data.begin(), Data.end(),
+        const double maxdata = std::abs(*std::max_element(Data.begin(), Data.end(),
             jiba::absLess<double, double>()));
         //create objects for the misfit and a very basic error estimate
         jiba::rvec DataError(ndata);
         for (size_t i = 0; i < ndata; ++i)
           {
-            DataError(i) = std::max(std::fabs(Data(i)), 1e-4 * maxdata)
-                * errorlevel;
+            DataError(i) = std::max(std::abs(Data(i)), absmin)
+                * relerror;
             assert(DataError(i) > 0.0);
           }
         return DataError;
