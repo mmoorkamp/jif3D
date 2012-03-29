8e34e2695927022d30e2d1a3005b32aa7be17d0c
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2012-03-29 10:46:22 +0000

    plotslatrel now produces a .vtk file to show the variation with all parameters adapted opttest to new setup routines

--- jiba++/Hacks/plotsaltrel.cpp
+++ jiba++/Hacks/plotsaltrel.cpp
@@ -7,42 +7,47 @@
 
 #include "../Inversion/ModelTransforms.h"
 #include "../Joint/SaltRelConstraint.h"
+#include "../ModelBase/VTKTools.h"
 #include <fstream>
 
-class DensTransform: public jiba::GeneralModelTransform
-  {
-public:
-  DensTransform()
-    {
-    }
-  virtual ~DensTransform()
-    {
-    }
-  virtual jiba::rvec GeneralizedToPhysical(const jiba::rvec &FullModel) const
-    {
-      jiba::rvec result(1);
-      result(0) = 2.2;
-      return result;
-    }
-  virtual jiba::rvec PhysicalToGeneralized(const jiba::rvec &FullModel) const
-    {
-      jiba::rvec result(1);
-      result(0) = 2.2;
-      return result;
-    }
-  virtual jiba::rvec Derivative(const jiba::rvec &FullModel,
-      const jiba::rvec &Derivative) const
-    {
-      jiba::rvec result(1);
-      result(0) = 0.0;
-      return result;
-    }
-  };
+//class DensTransform: public jiba::GeneralModelTransform
+//  {
+//public:
+//  DensTransform()
+//    {
+//    }
+//  virtual ~DensTransform()
+//    {
+//    }
+//  virtual jiba::rvec GeneralizedToPhysical(const jiba::rvec &FullModel) const
+//    {
+//      jiba::rvec result(1);
+//      result(0) = 2.2;
+//      return result;
+//    }
+//  virtual jiba::rvec PhysicalToGeneralized(const jiba::rvec &FullModel) const
+//    {
+//      jiba::rvec result(1);
+//      result(0) = 2.2;
+//      return result;
+//    }
+//  virtual jiba::rvec Derivative(const jiba::rvec &FullModel,
+//      const jiba::rvec &Derivative) const
+//    {
+//      jiba::rvec result(1);
+//      result(0) = 0.0;
+//      return result;
+//    }
+//  };
 
 int main()
   {
 
-    boost::shared_ptr<jiba::GeneralModelTransform> DensTrans(new DensTransform());
+    boost::shared_ptr<jiba::GeneralModelTransform> DensTrans(
+        new jiba::DensityTransform(
+            boost::shared_ptr<jiba::GeneralModelTransform>(
+                new jiba::ModelCopyTransform())));
+
     boost::shared_ptr<jiba::GeneralModelTransform> CondTrans(
         new jiba::ConductivityTransform(
             boost::shared_ptr<jiba::GeneralModelTransform>(
@@ -55,18 +60,45 @@ int main()
     double logmincond = -4;
     double logmaxcond = 0.5;
     double logcondstep = 0.05;
-    std::ofstream outfile("saltrel.dat");
+    double mindens = 1.5;
+    double maxdens = 4.0;
+    double densstep = 0.05;
+
+    const size_t nvel = (maxvel - minvel) / velstep +1;
+    const size_t nlogcond = (logmaxcond - logmincond) / logcondstep +1;
+    const size_t ndens = (maxdens - mindens) / densstep +1 ;
+
+    boost::multi_array<double, 1> VelAxis(boost::extents[nvel]), LogCondAxis(
+        boost::extents[nlogcond]), DensAxis(boost::extents[ndens]);
+    boost::multi_array<double, 3> Misfit(boost::extents[nvel][nlogcond][ndens]);
+    size_t currvelindex = 0, currcondindex = 0, currdensindex = 0;
     for (double vel = minvel; vel < maxvel; vel += velstep)
       {
+        VelAxis[currvelindex] = vel;
+
+        currcondindex = 0;
+
         for (double logcond = logmincond; logcond < logmaxcond; logcond += logcondstep)
           {
-            jiba::rvec Model(3);
-            Model(0) = 1.0 / vel;
-            Model(1) = 2.2;
-            Model(2) = pow(10, logcond);
-            double misfit = SaltRel.CalcMisfit(Model);
-            outfile << vel << " " << logcond << " " << misfit << std::endl;
-          }
-      }
+            LogCondAxis[currcondindex] = logcond;
 
+            currdensindex = 0;
+
+            for (double dens = mindens; dens < maxdens; dens += densstep)
+              {
+                DensAxis[currdensindex] = dens;
+                jiba::rvec Model(3);
+                Model(0) = 1.0 / vel;
+                Model(1) = dens;
+                Model(2) = pow(10, logcond);
+                Misfit[currvelindex][currcondindex][currdensindex] = SaltRel.CalcMisfit(
+                    Model);
+                ++currdensindex;
+              }
+            ++currcondindex;
+          }
+        ++currvelindex;
+      }
+    jiba::Write3DModelToVTK("saltrel.vtk", "SaltReal", VelAxis, LogCondAxis, DensAxis,
+        Misfit);
   }
