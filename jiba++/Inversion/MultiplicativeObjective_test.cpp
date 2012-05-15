18016c844e8c7a0c76f8bb04b5a94bb042598f8e
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2012-05-15 15:31:32 +0000

    added a naive implementation of multiplicative objective, does not currently work and is not build either

---
+++ jiba++/Inversion/MultiplicativeObjective_test.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : MultiplicativeObjective_test.cpp
+// Author      : 21 Mar 2012
+// Version     : 
+// Copyright   : 2012, mm489
+//============================================================================
+
+#include "MultiplicativeObjective.h"
+
+namespace jiba
+  {
+
+  } /* namespace jiba */
