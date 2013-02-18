2df16ed22a5f817406a5e82b7074e4abe0e3642a
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2013-02-18 10:22:03 +0000

    included more detailed error message in EqualGridGeometry

--- trunk/jiba++/ModelBase/EqualGeometry.h
+++ trunk/jiba++/ModelBase/EqualGeometry.h
@@ -5,47 +5,60 @@
 // Copyright   : 2010, mmoorkamp
 //============================================================================
 
-
 #ifndef EQUALGEOMETRY_H_
 #define EQUALGEOMETRY_H_
 
-#include "ThreeDModelBase.h"
+#include <iostream>
 #include "../Global/NumUtil.h"
+#include "../Global/FatalException.h"
+#include "ThreeDModelBase.h"
 
-namespace jiba
-  {
-    //! Test if two models have the same grid geometry
-    /*! This function can be used to test whether two 3D models have the same grid geometry, i.e. the same number of cells
-     * in each direction with the same sizes.
-     * @param Model1 The first model for the comparison
-     * @param Model2 the second model for the comparison
-     * @return True if the geometries are equal, false otherwise
-     */
-    inline bool EqualGridGeometry(const jiba::ThreeDModelBase &Model1,
-        const jiba::ThreeDModelBase &Model2)
-      {
-        //we first make a cheap test and then the more expensive ones
-        if (Model1.GetNModelElements() != Model2.GetNModelElements())
-          return false;
-        // as soon as one fails we return false
-        //we check the sizes of the cells in all three directions
-        if (!std::equal(Model1.GetXCellSizes().origin(),
-            Model1.GetXCellSizes().origin()
-                + Model1.GetXCellSizes().num_elements(),
-            Model2.GetXCellSizes().origin(),roughEqual<double,double>()))
-          return false;
-        if (!std::equal(Model1.GetYCellSizes().origin(),
-            Model1.GetYCellSizes().origin()
-                + Model1.GetYCellSizes().num_elements(),
-            Model2.GetYCellSizes().origin(),roughEqual<double,double>()))
-          return false;
-        if (!std::equal(Model1.GetZCellSizes().origin(),
-            Model1.GetZCellSizes().origin()
-                + Model1.GetZCellSizes().num_elements(),
-            Model2.GetZCellSizes().origin(),roughEqual<double,double>()))
-          return false;
-        //we only get here if everything is equal
-        return true;
-      }
-  }
+namespace jiba {
+//! Test if two models have the same grid geometry
+/*! This function can be used to test whether two 3D models have the same grid geometry, i.e. the same number of cells
+ * in each direction with the same sizes.
+ * @param Model1 The first model for the comparison
+ * @param Model2 the second model for the comparison
+ * @return True if the geometries are equal, false otherwise
+ */
+inline bool EqualGridGeometry(const jiba::ThreeDModelBase &Model1,
+		const jiba::ThreeDModelBase &Model2) {
+	//we first make a cheap test and then the more expensive ones
+	if (Model1.GetNModelElements() != Model2.GetNModelElements())
+		return false;
+	// as soon as one fails we return false
+	//we check the sizes of the cells in all three directions
+	const std::size_t nx = Model1.GetXCellSizes().num_elements();
+
+	for (std::size_t i = 0; i < nx; ++i) {
+		if (!roughlyEqual(Model1.GetXCellSizes()[i],
+				Model2.GetXCellSizes()[i])) {
+			std::cerr << "Cell sizes in x-direction do not match "
+					<< Model1.GetXCellSizes()[i] << " "
+					<< Model1.GetXCellSizes()[i] << std::endl;
+			return false;
+		}
+	}
+	for (std::size_t i = 0; i < nx; ++i) {
+		if (!roughlyEqual(Model1.GetYCellSizes()[i],
+				Model2.GetYCellSizes()[i])) {
+			std::cerr << "Cell sizes in y-direction do not match "
+					<< Model1.GetYCellSizes()[i] << " "
+					<< Model1.GetYCellSizes()[i] << std::endl;
+			return false;
+		}
+	}
+	for (std::size_t i = 0; i < nx; ++i) {
+		if (!roughlyEqual(Model1.GetZCellSizes()[i],
+				Model2.GetZCellSizes()[i])) {
+			std::cerr << "Cell sizes in z-direction do not match "
+					<< Model1.GetZCellSizes()[i] << " "
+					<< Model1.GetZCellSizes()[i] << std::endl;
+			return false;
+		}
+	}
+	//we only get here if everything is equal
+	return true;
+}
+}
 #endif /* EQUALGEOMETRY_H_ */
