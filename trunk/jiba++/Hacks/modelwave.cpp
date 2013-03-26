bdd451d9d0970aa760d216c286d7b0ef2b31aac7
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2013-03-26 14:32:50 +0000

    added some toy programs for wavelet regularization

---
+++ trunk/jiba++/Hacks/modelwave.cpp
@@ -0,0 +1,86 @@
+//============================================================================
+// Name        : modelwave.cpp
+// Author      : 5 Mar 2013
+// Version     : 
+// Copyright   : 2013, mm489
+//============================================================================
+
+#include <iostream>
+#include <algorithm>
+#include "../Tomo/ThreeDSeismicModel.h"
+#include "../Global/FileUtil.h"
+#include "../Global/Wavelet.h"
+#include "../Global/VecMat.h"
+#include "../ModelTransforms/WaveletModelTransform.h"
+
+int main()
+  {
+    jiba::ThreeDSeismicModel Model;
+    std::string InfileName = jiba::AskFilename("Input Model: ");
+    Model.ReadNetCDF(InfileName);
+
+    const size_t nx = Model.GetXCellSizes().size();
+    const size_t ny = Model.GetYCellSizes().size();
+    const size_t nz = Model.GetZCellSizes().size();
+
+    jiba::WaveletTransform(Model.SetSlownesses());
+
+    jiba::ThreeDSeismicModel WaveHigh(Model);
+    jiba::ThreeDSeismicModel WaveLow(Model);
+
+    for (size_t i = 3 * nx / 4; i < nx; ++i)
+      {
+        for (size_t j = 3 * ny / 4; j < ny; ++j)
+          {
+            for (size_t k = 3 * nz / 4; k < nz; ++k)
+              {
+                WaveHigh.SetData()[i][j][k] = 0;
+              }
+          }
+      }
+
+    for (size_t i = 0; i < nx / 4; ++i)
+      {
+        for (size_t j = 0; j < ny / 4; ++j)
+          {
+            for (size_t k = 0; k < nz / 4; ++k)
+              {
+                WaveLow.SetData()[i][j][k] = 0;
+              }
+          }
+      }
+
+    jiba::InvWaveletTransform(WaveHigh.SetSlownesses());
+    jiba::InvWaveletTransform(WaveLow.SetSlownesses());
+    WaveHigh.WriteNetCDF(InfileName + ".waveh.nc");
+    WaveLow.WriteNetCDF(InfileName + ".wavel.nc");
+    Model.WriteNetCDF(InfileName + ".wcoeff.nc");
+
+    jiba::rvec Elements(Model.GetSlownesses().num_elements());
+    std::copy(Model.GetSlownesses().origin(),
+        Model.GetSlownesses().origin() + Model.GetSlownesses().num_elements(),
+        Elements.begin());
+    const size_t nElem = Model.GetSlownesses().num_elements() / 1000;
+    std::nth_element(Elements.begin(), Elements.begin() + nElem, Elements.end(),
+        std::greater<double>());
+    std::cout << "The highest elements are: ";
+    std::copy(Elements.begin(), Elements.begin() + nElem,
+        std::ostream_iterator<double>(std::cout, " "));
+    std::cout << std::endl;
+    for (size_t i = 0; i < nx; ++i)
+      {
+        for (size_t j = 0; j < ny; ++j)
+          {
+            for (size_t k = 0; k < nz; ++k)
+              {
+                if (Model.GetData()[i][j][k] >= Elements(nElem - 1))
+                  {
+                    std::cout << i << " " << j << " " << k << " "
+                        << Model.GetData()[i][j][k] << std::endl;
+                  }
+
+              }
+          }
+      }
+  }
+
