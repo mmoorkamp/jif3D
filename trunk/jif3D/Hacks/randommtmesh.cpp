59949843fde4f663d30898ca2a64dd660766211b
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2013-06-23 21:42:15 +0000

    implemented different realizations within randommtmesh

--- trunk/jif3D/Hacks/randommtmesh.cpp
+++ trunk/jif3D/Hacks/randommtmesh.cpp
@@ -13,6 +13,7 @@
 #include <string>
 #include <cstdlib>
 #include "../Global/FileUtil.h"
+#include "../Global/convert.h"
 #include "../MT/X3DModel.h"
 #include "../MT/X3DMTCalculator.h"
 #include "../MT/ReadWriteImpedances.h"
@@ -65,27 +66,17 @@ int main()
 	std::cout << "Fraction Phase 1: ";
 	std::cin >> phase1frac;
 	srand48(time(0));
-	for (size_t i = 0; i < nx; ++i)
-	{
-		for (size_t j = 0; j < ny; ++j)
-		{
-			Model.SetConductivities()[i][j][0] = bg_conductivity;
-			for (size_t k = 1; k < nz; ++k)
-			{
-				if (drand48() < phase1frac)
-				{
-					Model.SetConductivities()[i][j][k] = phase1cond;
-				}
-				else
-				{
-					Model.SetConductivities()[i][j][k] = phase2cond;
-				}
-			}
-		}
-	}
+	double frequency;
+	std::cout << "Frequency: ";
+	std::cin >> frequency;
+	double posx, posy, posz = 0;
+	posx = (deltax * nx) / 2.0;
+	posy = (deltay * ny) / 2.0;
+	Model.SetFrequencies().assign(1, frequency);
+	Model.AddMeasurementPoint(posx, posy, posz);
 
 	//ask for a filename to write the mesh to
-	std::string MeshFilename = jif3D::AskFilename("Meshfile name: ", false);
+	std::string OutFilename = jif3D::AskFilename("Outfile name: ", false);
 	//we set the calculation frequencies to a dummy value
 	//the forward modeling program asks for those anyway
 
@@ -95,22 +86,50 @@ int main()
 			bg_conductivity);
 	Model.SetBackgroundThicknesses(bg_thicknesses);
 	Model.SetBackgroundConductivities(bg_conductivities);
-	Model.WriteNetCDF(MeshFilename);
-	Model.WriteVTK(MeshFilename + ".vtk");
-	double frequency;
-	std::cout << "Frequency: ";
-	std::cin >> frequency;
-	double posx, posy, posz = 0;
-	posx = (deltax * nx) / 2.0;
-	posy = (deltay * ny) / 2.0;
-	Model.SetFrequencies().assign(1, frequency);
-	Model.AddMeasurementPoint(posx,posy,posz);
-    std::cout << "Calculating forward response " << std::endl;
-    jif3D::X3DMTCalculator Calculator;
-    jif3D::rvec Impedances(Calculator.Calculate(Model));
-    jif3D::WriteImpedancesToNetCDF(MeshFilename+"_data.nc", Model.GetFrequencies(),
-        Model.GetMeasPosX(), Model.GetMeasPosY(), Model.GetMeasPosZ(), Impedances);
-    jif3D::rvec Errors(Impedances.size(),0.0);
-    jif3D::WriteImpedancesToMtt(MeshFilename+"_data.nc", Model.GetFrequencies(), Impedances, Errors);
+	size_t nrealmax;
+	std::cout << "Realizations: ";
+	std::cin >> nrealmax;
+
+	std::ofstream zxy("zxy.out");
+	std::ofstream zyx("zyx.out");
+	std::ofstream zxx("zxx.out");
+	std::ofstream zyy("zyy.out");
+	for (size_t nreal = 0; nreal < nrealmax; ++nreal)
+	{
+		std::cout << "Realization: " << nreal << std::endl;
+		for (size_t i = 0; i < nx; ++i)
+		{
+			for (size_t j = 0; j < ny; ++j)
+			{
+				Model.SetConductivities()[i][j][0] = bg_conductivity;
+				for (size_t k = 1; k < nz; ++k)
+				{
+					if (drand48() < phase1frac)
+					{
+						Model.SetConductivities()[i][j][k] = phase1cond;
+					}
+					else
+					{
+						Model.SetConductivities()[i][j][k] = phase2cond;
+					}
+				}
+			}
+		}
+		std::string realstring(jif3D::stringify(nreal));
+		Model.WriteNetCDF(OutFilename + realstring + ".nc");
+		jif3D::X3DMTCalculator Calculator;
+		jif3D::rvec Impedances(Calculator.Calculate(Model));
+		jif3D::WriteImpedancesToNetCDF(OutFilename + realstring + "_data.nc",
+				Model.GetFrequencies(), Model.GetMeasPosX(),
+				Model.GetMeasPosY(), Model.GetMeasPosZ(), Impedances);
+		jif3D::rvec Errors(Impedances.size(), 0.0);
+		jif3D::WriteImpedancesToMtt(OutFilename + realstring + "_data.nc",
+				Model.GetFrequencies(), Impedances, Errors);
+		Model.WriteVTK(OutFilename + realstring + ".vtk");
+		zxx << nreal << " " << Impedances(0) << " " << Impedances(1) << std::endl;
+		zxy << nreal << " " << Impedances(2) << " " << Impedances(3) << std::endl;
+		zyx << nreal << " " << Impedances(4) << " " << Impedances(5) << std::endl;
+		zyy << nreal << " " << Impedances(6) << " " << Impedances(7) << std::endl;
+	}
 
 }
