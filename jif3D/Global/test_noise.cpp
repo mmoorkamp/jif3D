49b94452975466949d9c8bac0234c288190ebe77
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2013-08-16 13:48:12 +0000

    added tests for properties of AddNoise

--- jif3D/Global/test_noise.cpp
+++ jif3D/Global/test_noise.cpp
@@ -22,34 +22,74 @@
 using namespace boost::accumulators;
 
 BOOST_AUTO_TEST_SUITE( Noise_Test_Suite )
+
+    BOOST_AUTO_TEST_CASE(relnoise)
+      {
+        const size_t ndata = 1e4;
+        jif3D::rvec Data(ndata);
+        std::fill_n(Data.begin(), ndata, 1.0);
+        const double relerror = 0.1;
+        jif3D::AddNoise(Data, relerror, 0.0);
+        for (double value : Data)
+          {
+            BOOST_CHECK(value != 1.0);
+          }
+      }
+
+    BOOST_AUTO_TEST_CASE(absnoise)
+      {
+        const size_t ndata = 1e4;
+        jif3D::rvec Data(ndata);
+        std::fill_n(Data.begin(), ndata, 1.0);
+        const double abserror = 0.1;
+        jif3D::AddNoise(Data, 0.0, abserror);
+        for (double value : Data)
+          {
+            BOOST_CHECK(value != 1.0);
+          }
+      }
+
+    BOOST_AUTO_TEST_CASE(zero)
+      {
+        const size_t ndata = 1e4;
+        jif3D::rvec Data(ndata);
+        std::fill_n(Data.begin(), ndata, 1.0);
+        jif3D::AddNoise(Data, 0.0, 0.0);
+        for (double value : Data)
+          {
+            BOOST_CHECK(value == 1.0);
+          }
+      }
+
 //check that the function to add noise to synthetic data
 //produces a Gaussian distribution with the required parameters
 //this test can fail occassionally due to the finite sample size
-BOOST_AUTO_TEST_CASE  (addnoise)
-    {
-      //we use a million data points
-      const size_t ndata = 1e6;
-      jif3D::rvec Data(ndata);
-      std::fill_n(Data.begin(),ndata,1.0);
-      jif3D::rvec Noisy(Data);
-      //test relative noise level
-      const double relerror = 0.1;
-      jif3D::AddNoise(Noisy,relerror,0.0);
-      jif3D::rvec Diff(Data-Noisy);
+    BOOST_AUTO_TEST_CASE (var)
+      {
+        //we use a million data points
+        const size_t ndata = 1e6;
+        jif3D::rvec Data(ndata);
+        std::fill_n(Data.begin(), ndata, 1.0);
+        jif3D::rvec Noisy(Data);
+        //test relative noise level
+        const double relerror = 0.1;
+        jif3D::AddNoise(Noisy, relerror, 0.0);
+        jif3D::rvec Diff(Data - Noisy);
 
-      accumulator_set<double, stats<tag::mean,tag::variance > > acc;
-      std::for_each( Diff.begin(), Diff.end(), boost::bind<void>( boost::ref(acc), _1 ) );
-      BOOST_CHECK(mean(acc) < 0.001);
-      BOOST_CHECK_CLOSE(variance(acc),relerror*relerror,0.2);
+        accumulator_set<double, stats<tag::mean, tag::variance> > acc;
+        std::for_each(Diff.begin(), Diff.end(), boost::bind<void>(boost::ref(acc), _1));
+        BOOST_CHECK(mean(acc) < 0.001);
+        BOOST_CHECK_CLOSE(variance(acc), relerror * relerror, 0.2);
 
-      //now check absolute noise level
-      Noisy = Data;
-      const double abserror = 1;
-      jif3D::AddNoise(Noisy,0.0,abserror);
-      Diff = Data - Noisy;
-      accumulator_set<double, stats<tag::mean,tag::variance > > acc2;
-      std::for_each( Diff.begin(), Diff.end(), boost::bind<void>( boost::ref(acc2), _1 ) );
-      BOOST_CHECK(mean(acc2) < 0.001);
-      BOOST_CHECK_CLOSE(variance(acc2),abserror*abserror,0.2);
-    }
-  BOOST_AUTO_TEST_SUITE_END()
+        //now check absolute noise level
+        Noisy = Data;
+        const double abserror = 1;
+        jif3D::AddNoise(Noisy, 0.0, abserror);
+        Diff = Data - Noisy;
+        accumulator_set<double, stats<tag::mean, tag::variance> > acc2;
+        std::for_each(Diff.begin(), Diff.end(), boost::bind<void>(boost::ref(acc2), _1));
+        BOOST_CHECK(mean(acc2) < 0.001);
+        BOOST_CHECK_CLOSE(variance(acc2), abserror * abserror, 0.2);
+      }
+
+    BOOST_AUTO_TEST_SUITE_END()
