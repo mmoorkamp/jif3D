cab4f02d60490471ceb8ec6b0cbcaffd59f39e26
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2013-09-06 13:55:05 +0000

    Added replacement for CUDA_SAFE_CALL in toolkit that is no longer distributed

---
+++ jif3D/Global/CudaError.h
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : CudaError.h
+// Author      : 6 Sep 2013
+// Version     : 
+// Copyright   : 2013, mm489
+//============================================================================
+
+
+#ifndef CUDAERROR_H_
+#define CUDAERROR_H_
+
+
+#define CUDA_SAFE_CALL(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+
+inline void gpuAssert(cudaError_t code, char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess)
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
+#endif /* CUDAERROR_H_ */
