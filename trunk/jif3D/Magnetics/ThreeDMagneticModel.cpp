be2d27fbda71dc1585d9a18f9bf0023543212b9d
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2013-10-11 16:51:43 +0000

    added magnetic models

---
+++ trunk/jif3D/Magnetics/ThreeDMagneticModel.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : ThreeDMagneticModel.cpp
+// Author      : 11 Oct 2013
+// Version     : 
+// Copyright   : 2013, mm489
+//============================================================================
+
+#include "ThreeDMagneticModel.h"
+
+namespace jif3D
+  {
+
+    ThreeDMagneticModel::ThreeDMagneticModel()
+      {
+        // TODO Auto-generated constructor stub
+
+      }
+
+    ThreeDMagneticModel::~ThreeDMagneticModel()
+      {
+        // TODO Auto-generated destructor stub
+      }
+
+  } /* namespace jif3D */
