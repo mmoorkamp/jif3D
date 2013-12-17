5e824229994a9be2bd76e2970d22655e49b5ea50
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2013-12-17 11:33:39 +0000

    added temporary directory option to hpx test

--- jif3D/hpxtest/CalcFreq.cpp
+++ jif3D/hpxtest/CalcFreq.cpp
@@ -22,7 +22,6 @@
 #include "../MT/MTEquations.h"
 #include "../ModelBase/CellBoundaries.h"
 
-
 namespace fs = boost::filesystem;
 
 //define some names that are always the same
@@ -44,8 +43,6 @@ const std::string dirext = "_dir";
 const std::map<jif3D::X3DModel::ProblemType, std::string> Extension =
     boost::assign::map_list_of(jif3D::X3DModel::MT, "MT")(jif3D::X3DModel::EDIP, "EDIP")(
         jif3D::X3DModel::MDIP, "MDIP");
-
-
 
 inline void CheckField(const std::vector<std::complex<double> > &Field, size_t nelem)
   {
@@ -121,7 +118,7 @@ void MakeRunFile(const std::string &NameRoot, const std::string DirName)
 void RunX3D(const std::string &NameRoot)
   {
     std::cout << "Running x3D from hpx " << std::endl;
-	//instead of making the script executable
+    //instead of making the script executable
     //we run a bash with the scriptname as an argument
     //this turns out to be more robust
 
@@ -136,94 +133,94 @@ void RunX3D(const std::string &NameRoot)
 jif3D::rvec HPXCalculateFrequency(const jif3D::X3DModel &Model, size_t freqindex,
     std::string TempDirName)
   {
-	std::cout << "Frequency: " <<  freqindex << " Running on: " << hpx::find_here() << " thread: " << hpx::get_worker_thread_num() << std::endl;
-	const size_t nmeas = Model.GetMeasPosX().size();
-	        const size_t nmodx = Model.GetXCoordinates().size();
-	        const size_t nmody = Model.GetYCoordinates().size();
-	        jif3D::rvec result(nmeas * 8);
-	        fs::path RootName =  MakeUniqueName(jif3D::X3DModel::MT, freqindex);
-	        fs::path DirName = RootName.string() + dirext;
-	        std::vector<double> CurrFreq(1, Model.GetFrequencies()[freqindex]);
-	        std::vector<double> ShiftDepth;
-	        std::vector<size_t> MeasDepthIndices;
-	        //construct a vector of indices of unique station depths
-	        size_t nlevels = ConstructDepthIndices(MeasDepthIndices, ShiftDepth, Model);
-	        //writing out files causes problems in parallel
-	        // so we make sure it is done one at a time
-	#pragma omp critical(forward_write_files)
-	          {
-	        	MakeRunFile(RootName.string(), DirName.string());
-	        	jif3D::WriteProjectFile(DirName.string(), CurrFreq, jif3D::X3DModel::MT, resultfilename,
-	                modelfilename);
-	        	jif3D::Write3DModelForX3D((DirName / modelfilename).string(), Model.GetXCellSizes(),
-	                Model.GetYCellSizes(), Model.GetZCellSizes(), ShiftDepth,
-	                Model.GetConductivities(), Model.GetBackgroundConductivities(),
-	                Model.GetBackgroundThicknesses());
-	          }
-	        //run x3d in parallel
-	        RunX3D(RootName.string());
-	        std::vector<std::complex<double> > Ex1, Ex2, Ey1, Ey2, Hx1, Hx2, Hy1, Hy2;
-	        std::complex<double> Zxx, Zxy, Zyx, Zyy;
-	        //read in the electric and magnetic field at the observe sites
-	#pragma omp critical(forward_read_emo)
-	          {
-	        	jif3D::ReadEMO((DirName / emoAname).string(), Ex1, Ey1, Hx1, Hy1);
-	        	jif3D::ReadEMO((DirName / emoBname).string(), Ex2, Ey2, Hx2, Hy2);
-	          }
-	        const size_t nval = (nmodx * nmody * nlevels);
-	       CheckField(Ex1, nval);
-	        CheckField(Ex2, nval);
-	        CheckField(Ey1, nval);
-	        CheckField(Ey2, nval);
-	        CheckField(Hx1, nval);
-	        CheckField(Hx2, nval);
-	        CheckField(Hy1, nval);
-	        CheckField(Hy2, nval);
-	        //calculate impedances from the field spectra for all measurement sites
-	        for (size_t j = 0; j < nmeas; ++j)
-	          {
-	            boost::array<jif3D::ThreeDModelBase::t3DModelData::index, 3> StationIndex =
-	                Model.FindAssociatedIndices(Model.GetMeasPosX()[j],
-	                    Model.GetMeasPosY()[j], Model.GetMeasPosZ()[j]);
-	// with the current equations we cannot use interpolation as the position
-	        	// of the source in the adjoint calculation is always smeared
-	        	//across the whole cell, this works best for a cell in the centre
-	        	//of the cell and any other position deteriorates convergence
-	//            std::complex<double> Ex1Inter = InterpolateField(Ex1, Model, j,
-	//                MeasDepthIndices);
-	//            std::complex<double> Ex2Inter = InterpolateField(Ex2, Model, j,
-	//                MeasDepthIndices);
-	//            std::complex<double> Ey1Inter = InterpolateField(Ey1, Model, j,
-	//                MeasDepthIndices);
-	//            std::complex<double> Ey2Inter = InterpolateField(Ey2, Model, j,
-	//                MeasDepthIndices);
-	//            std::complex<double> Hx1Inter = InterpolateField(Hx1, Model, j,
-	//                MeasDepthIndices);
-	//            std::complex<double> Hx2Inter = InterpolateField(Hx2, Model, j,
-	//                MeasDepthIndices);
-	//            std::complex<double> Hy1Inter = InterpolateField(Hy1, Model, j,
-	//                MeasDepthIndices);
-	//            std::complex<double> Hy2Inter = InterpolateField(Hy2, Model, j,
-	//                MeasDepthIndices);
-	//            FieldsToImpedance(Ex1Inter, Ex2Inter, Ey1Inter, Ey2Inter, Hx1Inter, Hx2Inter,
-	//                Hy1Inter, Hy2Inter, Zxx, Zxy, Zyx, Zyy);
-	            const size_t offset = (nmodx * nmody) * MeasDepthIndices[j] + StationIndex[0] * nmody
-	                + StationIndex[1];
-	            jif3D::FieldsToImpedance(Ex1[offset], Ex2[offset], Ey1[offset], Ey2[offset],
-	                Hx1[offset], Hx2[offset], Hy1[offset], Hy2[offset], Zxx, Zxy, Zyx, Zyy);
-	            //result is a local array for this frequency
-	            //so we can directly use it even in a threaded environment
-	            const size_t meas_index = j * 8;
-	            result(meas_index) = Zxx.real();
-	            result(meas_index + 1) = Zxx.imag();
-	            result(meas_index + 2) = Zxy.real();
-	            result(meas_index + 3) = Zxy.imag();
-	            result(meas_index + 4) = Zyx.real();
-	            result(meas_index + 5) = Zyx.imag();
-	            result(meas_index + 6) = Zyy.real();
-	            result(meas_index + 7) = Zyy.imag();
-	          }
-	        return result;
+    std::cout << "Frequency: " << freqindex << " Running on: " << hpx::find_here()
+        << " thread: " << hpx::get_worker_thread_num() << std::endl;
+    const size_t nmeas = Model.GetMeasPosX().size();
+    const size_t nmodx = Model.GetXCoordinates().size();
+    const size_t nmody = Model.GetYCoordinates().size();
+    jif3D::rvec result(nmeas * 8);
+    fs::path TempDir(TempDirName);
+    fs::path RootName = TempDir / MakeUniqueName(jif3D::X3DModel::MT, freqindex);
+    fs::path DirName = RootName.string() + dirext;
+    std::vector<double> CurrFreq(1, Model.GetFrequencies()[freqindex]);
+    std::vector<double> ShiftDepth;
+    std::vector<size_t> MeasDepthIndices;
+    //construct a vector of indices of unique station depths
+    size_t nlevels = ConstructDepthIndices(MeasDepthIndices, ShiftDepth, Model);
+    //writing out files causes problems in parallel
+    // so we make sure it is done one at a time
+
+    MakeRunFile(RootName.string(), DirName.string());
+    jif3D::WriteProjectFile(DirName.string(), CurrFreq, jif3D::X3DModel::MT,
+        resultfilename, modelfilename);
+    jif3D::Write3DModelForX3D((DirName / modelfilename).string(), Model.GetXCellSizes(),
+        Model.GetYCellSizes(), Model.GetZCellSizes(), ShiftDepth,
+        Model.GetConductivities(), Model.GetBackgroundConductivities(),
+        Model.GetBackgroundThicknesses());
+
+    //run x3d in parallel
+    RunX3D(RootName.string());
+    std::vector<std::complex<double> > Ex1, Ex2, Ey1, Ey2, Hx1, Hx2, Hy1, Hy2;
+    std::complex<double> Zxx, Zxy, Zyx, Zyy;
+    //read in the electric and magnetic field at the observe sites
+
+    jif3D::ReadEMO((DirName / emoAname).string(), Ex1, Ey1, Hx1, Hy1);
+    jif3D::ReadEMO((DirName / emoBname).string(), Ex2, Ey2, Hx2, Hy2);
+
+    const size_t nval = (nmodx * nmody * nlevels);
+    CheckField(Ex1, nval);
+    CheckField(Ex2, nval);
+    CheckField(Ey1, nval);
+    CheckField(Ey2, nval);
+    CheckField(Hx1, nval);
+    CheckField(Hx2, nval);
+    CheckField(Hy1, nval);
+    CheckField(Hy2, nval);
+    //calculate impedances from the field spectra for all measurement sites
+    for (size_t j = 0; j < nmeas; ++j)
+      {
+        boost::array<jif3D::ThreeDModelBase::t3DModelData::index, 3> StationIndex =
+            Model.FindAssociatedIndices(Model.GetMeasPosX()[j], Model.GetMeasPosY()[j],
+                Model.GetMeasPosZ()[j]);
+        // with the current equations we cannot use interpolation as the position
+        // of the source in the adjoint calculation is always smeared
+        //across the whole cell, this works best for a cell in the centre
+        //of the cell and any other position deteriorates convergence
+        //            std::complex<double> Ex1Inter = InterpolateField(Ex1, Model, j,
+        //                MeasDepthIndices);
+        //            std::complex<double> Ex2Inter = InterpolateField(Ex2, Model, j,
+        //                MeasDepthIndices);
+        //            std::complex<double> Ey1Inter = InterpolateField(Ey1, Model, j,
+        //                MeasDepthIndices);
+        //            std::complex<double> Ey2Inter = InterpolateField(Ey2, Model, j,
+        //                MeasDepthIndices);
+        //            std::complex<double> Hx1Inter = InterpolateField(Hx1, Model, j,
+        //                MeasDepthIndices);
+        //            std::complex<double> Hx2Inter = InterpolateField(Hx2, Model, j,
+        //                MeasDepthIndices);
+        //            std::complex<double> Hy1Inter = InterpolateField(Hy1, Model, j,
+        //                MeasDepthIndices);
+        //            std::complex<double> Hy2Inter = InterpolateField(Hy2, Model, j,
+        //                MeasDepthIndices);
+        //            FieldsToImpedance(Ex1Inter, Ex2Inter, Ey1Inter, Ey2Inter, Hx1Inter, Hx2Inter,
+        //                Hy1Inter, Hy2Inter, Zxx, Zxy, Zyx, Zyy);
+        const size_t offset = (nmodx * nmody) * MeasDepthIndices[j]
+            + StationIndex[0] * nmody + StationIndex[1];
+        jif3D::FieldsToImpedance(Ex1[offset], Ex2[offset], Ey1[offset], Ey2[offset],
+            Hx1[offset], Hx2[offset], Hy1[offset], Hy2[offset], Zxx, Zxy, Zyx, Zyy);
+        //result is a local array for this frequency
+        //so we can directly use it even in a threaded environment
+        const size_t meas_index = j * 8;
+        result(meas_index) = Zxx.real();
+        result(meas_index + 1) = Zxx.imag();
+        result(meas_index + 2) = Zxy.real();
+        result(meas_index + 3) = Zxy.imag();
+        result(meas_index + 4) = Zyx.real();
+        result(meas_index + 5) = Zyx.imag();
+        result(meas_index + 6) = Zyy.real();
+        result(meas_index + 7) = Zyy.imag();
+      }
+    return result;
   }
 
 HPX_REGISTER_PLAIN_ACTION(CalculateFrequency_action)
--- jif3D/hpxtest/test_HPXCalculator.cpp
+++ jif3D/hpxtest/test_HPXCalculator.cpp
@@ -8,6 +8,8 @@
 #include <hpx/config.hpp>
 #include <hpx/hpx_init.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/program_options.hpp>
+#include <boost/program_options/config.hpp>
 #include "../Global/NumUtil.h"
 #include "HPXMTCalculator.h"
 #include "../MT/X3DModel.h"
@@ -16,111 +18,124 @@
 #include "../MT/MT2DForward.h"
 #include "../MT/ReadWriteImpedances.h"
 
+namespace po = boost::program_options;
+
 int hpx_main(int argc, char* argv[])
-{
-	//create a 3D version of a 1D model
-	const size_t xsize = 20;
-	const size_t ysize = 20;
-	const size_t zsize = 10;
-	const size_t nbglayers = 5;
-	jif3D::X3DModel Model;
+  {
 
-	Model.SetZCellSizes().resize(boost::extents[zsize]);
+    po::options_description desc("General options");
+    desc.add_options()("help", "produce help message")("tempdir",
+        po::value<std::string>(),
+        "The name of the directory to store temporary files in");
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    boost::filesystem::path TempDir = boost::filesystem::current_path();
+    if (vm.count("tempdir"))
+      {
+        TempDir = vm["tempdir"].as<std::string>();
+        if (!boost::filesystem::is_directory(TempDir))
+          {
+            std::cerr << TempDir.string() << " is not a directory or does not exist ! \n";
+            return 500;
+          }
+      }
 
-	Model.SetConductivities().resize(boost::extents[xsize][ysize][zsize]);
-	std::vector<double> bg_thicknesses(nbglayers), bg_conductivities(nbglayers);
+    //create a 3D version of a 1D model
+    const size_t xsize = 20;
+    const size_t ysize = 20;
+    const size_t zsize = 10;
+    const size_t nbglayers = 5;
+    jif3D::X3DModel Model;
 
-	const double deltax = 100.0;
-	const double deltay = 100.0;
-	const double deltaz = 100.0;
-	const double freq = 1.0;
-	const size_t nfreq = 8;
-	const double cond = 0.01;
-	Model.SetHorizontalCellSize(deltax, deltay, xsize, ysize);
+    Model.SetZCellSizes().resize(boost::extents[zsize]);
 
-	std::fill_n(Model.SetZCellSizes().origin(), zsize, deltaz);
-	std::fill_n(Model.SetConductivities().origin(), xsize * ysize * zsize,
-			cond);
-	for (size_t i = 0; i < xsize * ysize * zsize; ++i)
-	{
-		*(Model.SetConductivities().origin() + i) *= i * 1.01;
-	}
-	std::fill_n(bg_conductivities.begin(), nbglayers, cond);
-	bg_conductivities.back() *= 1.001;
-	std::fill_n(bg_thicknesses.begin(), nbglayers, 200.0);
+    Model.SetConductivities().resize(boost::extents[xsize][ysize][zsize]);
+    std::vector<double> bg_thicknesses(nbglayers), bg_conductivities(nbglayers);
 
-	Model.SetBackgroundConductivities(bg_conductivities);
-	Model.SetBackgroundThicknesses(bg_thicknesses);
-	for (size_t i = 0; i < nfreq; ++i)
-	{
-		Model.SetFrequencies().push_back(freq * i + 1);
-	}
-	jif3D::X3DMTCalculator X3DCalculator;
-	jif3D::HPXMTCalculator HPXCalculator;
+    const double deltax = 100.0;
+    const double deltay = 100.0;
+    const double deltaz = 100.0;
+    const double freq = 1.0;
+    const size_t nfreq = 8;
+    const double cond = 0.01;
+    Model.SetHorizontalCellSize(deltax, deltay, xsize, ysize);
 
-	for (size_t i = 1; i < xsize / 2; ++i)
-		for (size_t j = 1; j < ysize / 2; ++j)
-		{
-			Model.AddMeasurementPoint(Model.GetXCoordinates()[i] + deltax / 2.0,
-					Model.GetYCoordinates()[j] + deltay / 2.0, 0.0);
-		}
-	std::cout << "X3D " << std::endl;
-	boost::posix_time::ptime x3dstarttime =
-			boost::posix_time::microsec_clock::local_time();
-	jif3D::rvec X3DImpedance = X3DCalculator.Calculate(Model);
-	boost::posix_time::ptime x3dendtime =
-			boost::posix_time::microsec_clock::local_time();
-	std::cout << "HPX " << std::endl;
-	boost::posix_time::ptime hpxstarttime =
-			boost::posix_time::microsec_clock::local_time();
-	jif3D::rvec HPXImpedance = HPXCalculator.Calculate(Model);
-	boost::posix_time::ptime hpxendtime =
-			boost::posix_time::microsec_clock::local_time();
-	std::complex<double> HsImp = jif3D::ImpedanceHalfspace(freq, cond);
-	const double prec = 0.05;
-	const size_t nsites = HPXImpedance.size() / 8;
-	for (size_t i = 0; i < nsites; ++i)
-	{
-		if (std::abs(
-				(HPXImpedance(i * 8 + 2) - X3DImpedance(i * 8 + 2))
-						/ X3DImpedance(i * 8 + 2)) > 0.01
-				|| std::abs(
-						(HPXImpedance(i * 8 + 3) - X3DImpedance(i * 8 + 3))
-								/ X3DImpedance(i * 8 + 3)) > 0.01
-				|| std::abs(
-						(HPXImpedance(i * 8 + 4) - X3DImpedance(i * 8 + 4))
-								/ X3DImpedance(i * 8 + 4)) > 0.01
-				|| std::abs(
-						(HPXImpedance(i * 8 + 5) - X3DImpedance(i * 8 + 5))
-								/ X3DImpedance(i * 8 + 5)) > 0.01)
-		{
-			std::cout << HPXImpedance(i * 8 + 2) << " "
-					<< X3DImpedance(i * 8 + 2) << "     "
-					<< HPXImpedance(i * 8 + 3) << " " << X3DImpedance(i * 8 + 3)
-					<< std::endl;
-			std::cout << HPXImpedance(i * 8 + 4) << " "
-					<< X3DImpedance(i * 8 + 4) << "     "
-					<< HPXImpedance(i * 8 + 5) << " " << X3DImpedance(i * 8 + 5)
-					<< std::endl;
-		}
-	}
-	std::cout << " Times: " << std::endl;
-	std::cout << "x3d: " << (x3dendtime - x3dstarttime).total_microseconds()
-			<< std::endl;
-	std::cout << "hpx: " << (hpxendtime - hpxstarttime).total_microseconds()
-			<< std::endl;
-	// Any HPX application logic goes here...
-	return hpx::finalize();
-	return 1;
-}
+    std::fill_n(Model.SetZCellSizes().origin(), zsize, deltaz);
+    std::fill_n(Model.SetConductivities().origin(), xsize * ysize * zsize, cond);
+    for (size_t i = 0; i < xsize * ysize * zsize; ++i)
+      {
+        *(Model.SetConductivities().origin() + i) *= i * 1.01;
+      }
+    std::fill_n(bg_conductivities.begin(), nbglayers, cond);
+    bg_conductivities.back() *= 1.001;
+    std::fill_n(bg_thicknesses.begin(), nbglayers, 200.0);
+
+    Model.SetBackgroundConductivities(bg_conductivities);
+    Model.SetBackgroundThicknesses(bg_thicknesses);
+    for (size_t i = 0; i < nfreq; ++i)
+      {
+        Model.SetFrequencies().push_back(freq * i + 1);
+      }
+    jif3D::X3DMTCalculator X3DCalculator(TempDir);
+    jif3D::HPXMTCalculator HPXCalculator(TempDir);
+
+    for (size_t i = 1; i < xsize / 2; ++i)
+      for (size_t j = 1; j < ysize / 2; ++j)
+        {
+          Model.AddMeasurementPoint(Model.GetXCoordinates()[i] + deltax / 2.0,
+              Model.GetYCoordinates()[j] + deltay / 2.0, 0.0);
+        }
+    std::cout << "X3D " << std::endl;
+    boost::posix_time::ptime x3dstarttime =
+        boost::posix_time::microsec_clock::local_time();
+    jif3D::rvec X3DImpedance = X3DCalculator.Calculate(Model);
+    boost::posix_time::ptime x3dendtime = boost::posix_time::microsec_clock::local_time();
+    std::cout << "HPX " << std::endl;
+    boost::posix_time::ptime hpxstarttime =
+        boost::posix_time::microsec_clock::local_time();
+    jif3D::rvec HPXImpedance = HPXCalculator.Calculate(Model);
+    boost::posix_time::ptime hpxendtime = boost::posix_time::microsec_clock::local_time();
+    std::complex<double> HsImp = jif3D::ImpedanceHalfspace(freq, cond);
+    const double prec = 0.05;
+    const size_t nsites = HPXImpedance.size() / 8;
+    for (size_t i = 0; i < nsites; ++i)
+      {
+        if (std::abs(
+            (HPXImpedance(i * 8 + 2) - X3DImpedance(i * 8 + 2)) / X3DImpedance(i * 8 + 2))
+            > 0.01
+            || std::abs(
+                (HPXImpedance(i * 8 + 3) - X3DImpedance(i * 8 + 3))
+                    / X3DImpedance(i * 8 + 3)) > 0.01
+            || std::abs(
+                (HPXImpedance(i * 8 + 4) - X3DImpedance(i * 8 + 4))
+                    / X3DImpedance(i * 8 + 4)) > 0.01
+            || std::abs(
+                (HPXImpedance(i * 8 + 5) - X3DImpedance(i * 8 + 5))
+                    / X3DImpedance(i * 8 + 5)) > 0.01)
+          {
+            std::cout << HPXImpedance(i * 8 + 2) << " " << X3DImpedance(i * 8 + 2)
+                << "     " << HPXImpedance(i * 8 + 3) << " " << X3DImpedance(i * 8 + 3)
+                << std::endl;
+            std::cout << HPXImpedance(i * 8 + 4) << " " << X3DImpedance(i * 8 + 4)
+                << "     " << HPXImpedance(i * 8 + 5) << " " << X3DImpedance(i * 8 + 5)
+                << std::endl;
+          }
+      }
+    std::cout << " Times: " << std::endl;
+    std::cout << "x3d: " << (x3dendtime - x3dstarttime).total_microseconds() << std::endl;
+    std::cout << "hpx: " << (hpxendtime - hpxstarttime).total_microseconds() << std::endl;
+    // Any HPX application logic goes here...
+    return hpx::finalize();
+    return 1;
+  }
 
 int main(int argc, char* argv[])
-{
+  {
 
-	// Initialize HPX, run hpx_main as the first HPX thread, and
-	// wait for hpx::finalize being called.
+    // Initialize HPX, run hpx_main as the first HPX thread, and
+    // wait for hpx::finalize being called.
 
-	return hpx::init(argc, argv);
+    return hpx::init(argc, argv);
 
-}
+  }
 
