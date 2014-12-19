3b0f1e1dd6a6c39162b08f9401a1312612b8fc6d
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2014-12-19 14:45:22 +0000

    continued implementation of python interface for simer code, first working version

---
+++ jif3D/hpx_simer/InvertBlock.h
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : InvertBlock.h
+// Author      : 19 Dec 2014
+// Version     : 
+// Copyright   : 2014, mm489
+//============================================================================
+
+#ifndef HPX_SIMER_INVERTBLOCK_H_
+#define HPX_SIMER_INVERTBLOCK_H_
+
+
+#include <vector>
+#include <string>
+#include "../Global/VecMat.h"
+#include "../MT/X3DModel.h"
+#include "realinfo.h"
+
+double InvertBlock(jif3D::X3DModel Model, jif3D::rvec Data,  std::string tempdir, std::string x3dname);
+
+
+#endif /* HPX_SIMER_INVERTBLOCK_H_ */
