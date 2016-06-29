4d7a1986d9733865dbf66d47c412e7154f832344
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2016-06-29 08:39:02 +0000

    added some further constraints when importing travel time picks from ascii data, some changes to how MT ascii data is read

--- trunk/jif3D/Joint/jointinv.cpp
+++ trunk/jif3D/Joint/jointinv.cpp
@@ -619,6 +619,11 @@ int main(int argc, char* argv[])
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
+    if (vm.count("help"))
+      {
+        std::cout << desc << "\n";
+        return 1;
+      }
     return hpx_main(vm);
 #endif
 
