ea86b66cef13a8e86d720d363a8a8006a7c9dbcd
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2021-03-02 15:01:38 +0000

    improved equal geometry check for different models to better deal with floating point comparisons, precision is now 1cm

--- jif3D/ModelBase/EqualGeometry.h
+++ jif3D/ModelBase/EqualGeometry.h
@@ -20,7 +20,7 @@ namespace jif3D
     /* @{ */
     //! Test if two models have the same grid geometry
     /*! This function can be used to test whether two 3D models have the same grid geometry, i.e. the same number of cells
-     * in each direction with the same sizes.
+     * in each direction with the same sizes. It also checks if the origins match
      * @param Model1 The first model for the comparison
      * @param Model2 the second model for the comparison
      * @return True if the geometries are equal, false otherwise
@@ -35,12 +35,37 @@ namespace jif3D
                 << " " << Model2.GetNModelElements() << std::endl;
             return false;
           }
+        //compare the origin of the models to a precision of 1cm
+        const double tolerance = 0.01;
+        if (!roughlyEqual<double, double>(tolerance)(Model1.GetXOrigin(),
+            Model2.GetXOrigin()))
+          {
+            std::cerr << "Models have different x-origins: " << Model1.GetXOrigin() << " "
+                << Model2.GetXOrigin() << std::endl;
+            return false;
+          }
+
+        if (!roughlyEqual<double, double>(tolerance)(Model1.GetYOrigin(),
+            Model2.GetYOrigin()))
+          {
+            std::cerr << "Models have different y-origins: " << Model1.GetYOrigin() << " "
+                << Model2.GetYOrigin() << std::endl;
+            return false;
+          }
+
+        if (!roughlyEqual<double, double>(tolerance)(Model1.GetZOrigin(),
+            Model2.GetZOrigin()))
+          {
+            std::cerr << "Models have different z-origins: " << Model1.GetZOrigin() << " "
+                << Model2.GetZOrigin() << std::endl;
+            return false;
+          }
         // as soon as one fails we return false
         //we check the sizes of the cells in all three directions
         const std::size_t nx = Model1.GetXCellSizes().size();
         const std::size_t ny = Model1.GetYCellSizes().size();
         const std::size_t nz = Model1.GetZCellSizes().size();
-
+        //check if we have the same number of cells in each direction
         if (nx != Model2.GetXCellSizes().size())
           {
             std::cerr << "Different number of cells in x-direction " << nx << " "
@@ -61,10 +86,13 @@ namespace jif3D
                 << Model2.GetZCellSizes().size() << std::endl;
             return false;
           }
+        //now check all the cell sizes, given the issues with floating point
+        //comparisons we cannot simply do a straight comparison, so we do an approximate one
+        //for our models a match within 1cm should be more than sufficient
 
         for (std::size_t i = 0; i < nx; ++i)
           {
-            if (!roughlyEqual<double, double>()(Model1.GetXCellSizes()[i],
+            if (!roughlyEqual<double, double>(tolerance)(Model1.GetXCellSizes()[i],
                 Model2.GetXCellSizes()[i]))
               {
                 std::cerr << "Cell sizes in x-direction do not match "
@@ -75,7 +103,7 @@ namespace jif3D
           }
         for (std::size_t i = 0; i < ny; ++i)
           {
-            if (!roughlyEqual<double, double>()(Model1.GetYCellSizes()[i],
+            if (!roughlyEqual<double, double>(tolerance)(Model1.GetYCellSizes()[i],
                 Model2.GetYCellSizes()[i]))
               {
                 std::cerr << "Cell sizes in y-direction do not match "
@@ -86,7 +114,7 @@ namespace jif3D
           }
         for (std::size_t i = 0; i < nz; ++i)
           {
-            if (!roughlyEqual<double, double>()(Model1.GetZCellSizes()[i],
+            if (!roughlyEqual<double, double>(tolerance)(Model1.GetZCellSizes()[i],
                 Model2.GetZCellSizes()[i]))
               {
                 std::cerr << "Cell sizes in z-direction do not match "
