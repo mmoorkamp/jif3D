988393265be73f55851542d060897b17cbdbb62b
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2022-05-20 09:49:14 +0000

    updated gravity read write test, streamlined formatting

--- jif3D/Gravity/test_readwritedata.cpp
+++ jif3D/Gravity/test_readwritedata.cpp
@@ -18,56 +18,68 @@ BOOST_AUTO_TEST_SUITE (GravityReadWrite_Test_Suite)
 
 //check whether writing data to a file and reading it in again
 //gives the same data back for the scalar case
-BOOST_AUTO_TEST_CASE(readwrite_scalar_test)
-  {
-    const size_t ndata = 20;
-    std::vector<double> Data(ndata), Error(ndata);
-    std::vector<double> PosX(ndata, 0.0), PosY(ndata, 0.0), PosZ(ndata, 0.0);
-    std::generate_n(Data.begin(), ndata, rand);
-    std::generate_n(Error.begin(), ndata, rand);
-    std::generate_n(PosX.begin(), ndata, rand);
-    std::generate_n(PosY.begin(), ndata, rand);
-    std::generate_n(PosZ.begin(), ndata, rand);
-    jif3D::ScalarGravityData ScalData;
-    ScalData.SetMeasurementPoints(PosX,PosY,PosZ);
-    ScalData.SetDataAndErrors(Data,Error);
-    std::string filename("scalar.nc");
-    ScalData.WriteNetCDF(filename);
-    jif3D::ScalarGravityData CompData;
-    CompData.ReadNetCDF("scalar.nc");
+    BOOST_AUTO_TEST_CASE(readwrite_scalar_test)
+      {
+        const size_t ndata = 20;
+        std::vector<double> Data(ndata), Error(ndata);
+        std::vector<double> PosX(ndata, 0.0), PosY(ndata, 0.0), PosZ(ndata, 0.0);
+        std::generate_n(Data.begin(), ndata, rand);
+        std::generate_n(Error.begin(), ndata, rand);
+        std::generate_n(PosX.begin(), ndata, rand);
+        std::generate_n(PosY.begin(), ndata, rand);
+        std::generate_n(PosZ.begin(), ndata, rand);
+        jif3D::ScalarGravityData ScalData;
+        ScalData.SetMeasurementPoints(PosX, PosY, PosZ);
+        ScalData.SetDataAndErrors(Data, Error);
+        std::string filename("scalar.nc");
+        ScalData.WriteNetCDF(filename);
+        jif3D::ScalarGravityData CompData;
+        CompData.ReadNetCDF("scalar.nc");
 
-    BOOST_CHECK(std::equal(ScalData.GetData().begin(), ScalData.GetData().end(),CompData.GetData().begin()));
-    BOOST_CHECK(std::equal(ScalData.GetErrors().begin(), ScalData.GetErrors().end(),CompData.GetErrors().begin()));
-    BOOST_CHECK(std::equal(ScalData.GetMeasPosX().begin(), ScalData.GetMeasPosX().end(),CompData.GetMeasPosX().begin()));
-    BOOST_CHECK(std::equal(ScalData.GetMeasPosY().begin(), ScalData.GetMeasPosY().end(),CompData.GetMeasPosY().begin()));
-    BOOST_CHECK(std::equal(ScalData.GetMeasPosZ().begin(), ScalData.GetMeasPosZ().end(),CompData.GetMeasPosZ().begin()));
-    BOOST_CHECK(jif3D::IdentifyGravityDatafileType(filename) == jif3D::scalar);
-  }
+        BOOST_TEST(ScalData.GetData() == CompData.GetData(),
+            boost::test_tools::per_element());
+        BOOST_TEST(ScalData.GetErrors() == CompData.GetErrors(),
+            boost::test_tools::per_element());
+        BOOST_TEST(ScalData.GetMeasPosX() == CompData.GetMeasPosX(),
+            boost::test_tools::per_element());
+        BOOST_TEST(ScalData.GetMeasPosY() == CompData.GetMeasPosY(),
+            boost::test_tools::per_element());
+        BOOST_TEST(ScalData.GetMeasPosZ() == CompData.GetMeasPosZ(),
+            boost::test_tools::per_element());
+
+        BOOST_CHECK(jif3D::IdentifyGravityDatafileType(filename) == jif3D::scalar);
+      }
 
 //check whether writing data to a file and reading it in again
 //gives the same data back for the tensor case
-BOOST_AUTO_TEST_CASE(readwrite_tensor_test)
-  {
-    const size_t nmeas =20;
-    const size_t ndata = nmeas * 9;
-    std::vector<double> Data(ndata), Error(ndata);
-    std::vector<double> PosX(nmeas, 0.0), PosY(nmeas, 0.0), PosZ(nmeas, 0.0);
-    std::generate_n(Data.begin(), ndata, rand);
-    std::generate_n(Error.begin(), ndata, rand);
-    std::generate_n(PosX.begin(), nmeas, rand);
-    std::generate_n(PosY.begin(), nmeas, rand);
-    std::generate_n(PosZ.begin(), nmeas, rand);
-    std::string filename("tensor.nc");
-    jif3D::SaveTensorGravityMeasurements(filename, Data, PosX, PosY, PosZ, Error);
-    std::vector<double> NewData(ndata), NewError(ndata);
-    std::vector<double> NewPosX(nmeas, 0.0), NewPosY(nmeas, 0.0), NewPosZ(nmeas, 0.0);
-    jif3D::ReadTensorGravityMeasurements(filename, NewData, NewPosX, NewPosY, NewPosZ,
-        NewError);
-    BOOST_CHECK(std::equal(Data.begin(), Data.end(),NewData.begin()));
-    BOOST_CHECK(std::equal(Error.begin(), Error.end(),NewError.begin()));
-    BOOST_CHECK(std::equal(PosX.begin(), PosX.end(),NewPosX.begin()));
-    BOOST_CHECK(std::equal(PosY.begin(), PosY.end(),NewPosY.begin()));
-    BOOST_CHECK(std::equal(PosZ.begin(), PosZ.end(),NewPosZ.begin()));
-    BOOST_CHECK(jif3D::IdentifyGravityDatafileType(filename) == jif3D::ftg);
-  }
-BOOST_AUTO_TEST_SUITE_END()
+    BOOST_AUTO_TEST_CASE(readwrite_tensor_test)
+      {
+        const size_t nmeas = 20;
+        const size_t ndata = nmeas * 9;
+        std::vector<double> Data(ndata), Error(ndata);
+        std::vector<double> PosX(nmeas, 0.0), PosY(nmeas, 0.0), PosZ(nmeas, 0.0);
+        std::generate_n(Data.begin(), ndata, rand);
+        std::generate_n(Error.begin(), ndata, rand);
+        std::generate_n(PosX.begin(), nmeas, rand);
+        std::generate_n(PosY.begin(), nmeas, rand);
+        std::generate_n(PosZ.begin(), nmeas, rand);
+        jif3D::TensorGravityData TensData;
+        TensData.SetMeasurementPoints(PosX, PosY, PosZ);
+        TensData.SetDataAndErrors(Data, Error);
+        std::string filename("tensor.nc");
+        TensData.WriteNetCDF(filename);
+        jif3D::TensorGravityData CompData;
+        CompData.ReadNetCDF(filename);
+        BOOST_TEST(TensData.GetData() == CompData.GetData(),
+            boost::test_tools::per_element());
+        BOOST_TEST(TensData.GetErrors() == CompData.GetErrors(),
+            boost::test_tools::per_element());
+        BOOST_TEST(TensData.GetMeasPosX() == CompData.GetMeasPosX(),
+            boost::test_tools::per_element());
+        BOOST_TEST(TensData.GetMeasPosY() == CompData.GetMeasPosY(),
+            boost::test_tools::per_element());
+        BOOST_TEST(TensData.GetMeasPosZ() == CompData.GetMeasPosZ(),
+            boost::test_tools::per_element());
+        BOOST_CHECK(jif3D::IdentifyGravityDatafileType(filename) == jif3D::ftg);
+      }
+    BOOST_AUTO_TEST_SUITE_END()
