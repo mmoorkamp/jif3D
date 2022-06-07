87541d16abb86165cf3bdf095c4eeb724dd27013
mmoorkamp <mmoorkamp@48fc5444-86aa-4d21-a708-6e26856e7b29>
2022-06-07 14:23:27 +0000

    Made fractals example compilable under new hpx

--- jif3D/Global/Serialization.h
+++ jif3D/Global/Serialization.h
@@ -10,16 +10,10 @@
 
 #ifdef HAVEHPX
 #include <hpx/config.hpp>
-#include <hpx/runtime/serialization/serialize.hpp>
-#include <hpx/runtime/serialization/base_object.hpp>
-#include <hpx/runtime/serialization/vector.hpp>
-#include <hpx/runtime/serialization/array.hpp>
-#include <hpx/runtime/serialization/complex.hpp>
-#include <hpx/runtime/serialization/shared_ptr.hpp>
-#include <hpx/runtime/serialization/serialization_fwd.hpp>
+#include <hpx/serialization.hpp>
 using hpx::serialization::base_object;
 using hpx::serialization::access;
-using hpx::serialization::make_array
+using hpx::serialization::make_array;
 #else
 #include <boost/serialization/serialization.hpp>
 #include <boost/serialization/base_object.hpp>
